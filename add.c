a2c9b0801d34f754cedaf9f693151798d5f8cf6a
saruboss0108 <46436393+saruboss0108@users.noreply.github.com>
2019-01-11 10:01:03 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,50 +1,29 @@
 #include<stdio.h>
+void input(int *a,int *b)
+{
+printf("Enter 1st number\n");
+scanf("%d",a);
+printf("Enter 2nd number\n");
+scanf("%d",b);
+}
+int add(int a,int b)
+{
+int c;
+c=a+b;
+return c;
+}
+void output(int a,int b,int c)
+{
+printf("%d + %d = %d",a,b,c);
+}
 
-struct add{
-    int a,b;
-};
-/*int input(struct add* q)
+int main()
 {
-   printf("enter 2 numbers\n");
-   scanf(" %d %d", &q->a, &q->b);
-}*/
-int input(void)
-{
-   struct add q;
-   printf("enter 1st number\n");
-   scanf("%d",&q.a);
-   printf("a = %d\n",q.a);
-   printf("enter 2nd number\n");
-   scanf("%d",&q.b);
-   printf("b = %d\n",q.b);
-   int c = compute(q);
+int x,y,z;
+input(&x,&y);
+    z=add(x,y);
+output(x,y,z);
 }
-/*int input2(void)
-{
-   int b;
-   scanf("%d",&b);
-   return b;
-}*/
-int compute(struct add q)
-{
-    int c = q.a+q.b;
-    return c;
-}
-void output(int m)
-{
-    printf(" sum = %d", m);
-}
-/*int main(void)
-{   struct add q;
-    input(&(q));
-    int n=compute(q.a, q.b);
-    printf(" a=%d, b=%d\n",q.a, q.b);
-    output(n);
-}*/
-int main(void)
-{
-     struct add q;
-    int a = input();
-    output(a);
-}
+
+                         
 
